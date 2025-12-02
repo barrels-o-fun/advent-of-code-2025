98edcf4626af3cd1fbbe026de1a84667a11c5ce9
GarethW <weirdybeardy@gmail.com>
2025-12-02 18:49:56 +0000

    day1 pt1 complete

--- day1/safe.js
+++ day1/safe.js
@@ -1,8 +1,8 @@
 import { createReadStream } from 'node:fs';
 import { createInterface } from 'node:readline';
 
-// const inFile = '/home/gwarren/code/advent-of-code-2025/day1/testData'
-const inFile = '/home/gwarren/code/advent-of-code-2025/day1/input'
+const inFile = '/home/gwarren/code/advent-of-code-2025/day1/testData'
+// const inFile = '/home/gwarren/code/advent-of-code-2025/day1/input'
 
 // copied from: 
 // https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
@@ -27,35 +27,49 @@ async function processLineByLine() {
 
 // check result of rotation and account for boundarys (0 - 99)
 // did not account for rotations greater than 100...
-function rotationResult(curPos, input) {
+function rotationResult(curPos, input, safeSize) {
     let result;
     // If L - minus
     // if R - add
-    if (input.includes('L')) {
-        let clean = input.slice(1)
-        result = curPos - Number(clean)
-    } else if (input.includes('R')) {
-        let clean = input.slice(1)
-        result = curPos + Number(clean)
+    if (!input.includes('L') && !input.includes('R')) {
+        // Invalid data
+        return curPos;
     }
+
+    if (input.includes('L')) {
+        let num = input.slice(1)
+        result = curPos - Number(num)
+    } else if (input.includes('R')) {
+        let num = input.slice(1)
+        result = curPos + Number(num)
+    }
+
     // Check if past boundary
     if (result < 0) {
-        result = 100 + result
+        result = safeSize + (result % safeSize)
     } else if (result > 99) {
-        result = result - 100
+        result = (result % safeSize) - safeSize
+    }
+
+    if (result == safeSize || result == -safeSize) {
+        result = 0;
     }
 
     // return result
     return result
 }
 
+// Init
+const startPos = 50;
+const safeSize = 100;
 const rotationsArr = await processLineByLine();
+
+// Processing
 let zeroFound = 0;
-const startPos = 50
 let position = startPos
 
 for (let line of rotationsArr) {
-    position = rotationResult(position, line)
+    position = rotationResult(position, line, safeSize)
     if (position == 0) {
         zeroFound++;
     }
